3a9c36db69bad4f0c61d6879b2c8c069591f46da
jnlongnecker <jlnecker1@gmail.com>
2021-12-08 20:52:43 -0500

    Initial noodling and figuring out vs code extensions

---
+++ extension.js
@@ -0,0 +1,57 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const cp = require('child_process');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Anonymous Apex Executer Activated');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('anonymous-executor.executeAnonymous', function () {
+		// The code you place here will be executed every time your command is executed
+
+		vscode.workspace.fs.readDirectory(vscode.workspace.workspaceFolders[0].uri).then(response => {
+			console.log("Logging Result");
+			console.log(response);
+		});
+
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: "Running Anonymous Apex"
+		}, () => execShell('echo testing', vscode.window.showInformationMessage));
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+const execShell = (/** @type {string} */ cmd, callback) =>
+	new Promise((resolve, reject) => {
+		cp.exec(cmd, (err, out) => {
+			if (err) {
+				callback(err);
+				return reject(err);
+			}
+			callback(out);
+			return resolve(out);
+		});
+	});
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
